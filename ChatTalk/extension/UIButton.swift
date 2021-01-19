345241c60607b1f08dee7721cf0f9c291db0da72
kimyejin <7602kyj@e-mirim.hs.kr>
2021-01-19 23:40:00 +0900

    UIButton 확장 및 회원가입 버튼 추가

---
+++ ChatTalk/extension/UIButton.swift
@@ -0,0 +1,41 @@
+//
+//  UIButton.swift
+//  BucketList
+//
+//  Created by 김예진 on 2021/01/05.
+//
+
+import UIKit
+
+// UIButton을 확장해 스토리보드 안에서 UI를 추가적으로 꾸밀 수 있다
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
