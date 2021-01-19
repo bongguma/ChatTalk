521dfe6032d030fb85b49e4fc3e8e4118367cb31
kimyejin <7602kyj@e-mirim.hs.kr>
2021-01-19 23:28:56 +0900

    로그인 화면 storyboard 작업 및 viewController 생성

---
+++ ChatTalk/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  ChatTalk
+//
+//  Created by 김예진 on 2021/01/19.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    let remoteConfig = RemoteConfig.remoteConfig()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
