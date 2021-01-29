83b04146e517b65337aa7bfc80bd56a7cacbe5e2
kimyejin <7602kyj@e-mirim.hs.kr>
2021-01-29 23:37:37 +0900

    userModal data 생성

---
+++ ChatTalk/Data/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  ChatTalk
+//
+//  Created by 김예진 on 2021/01/29.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+    var profileImage : String?
+    
+    var userName : String?
+}
