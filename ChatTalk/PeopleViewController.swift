f998bc4c58a0abf7c10549637639de54bedfb60a
kimyejin <7602kyj@e-mirim.hs.kr>
2021-01-29 23:37:14 +0900

    친구목록 tableview 기능 구현

---
+++ ChatTalk/PeopleViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MainViewController.swift
+//  ChatTalk
+//
+//  Created by 김예진 on 2021/01/25.
+//
+
+import UIKit
+import Firebase
+
+class PeopleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var peopleInfoArr:[UserModel] = []
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peopleInfoArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let peopleInfoCell = tableView.dequeueReusableCell(withIdentifier: "peopleInfoCell", for:indexPath)
+        
+        return peopleInfoCell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "peopleInfoCell")
+        
+        self.peopleInfoArr.removeAll()
+        
+        Database.database().reference().child("users").observe(DataEventType.value) { (snapshot) in
+            for child in snapshot.children {
+                let snapChild = child as! DataSnapshot
+                let userModel = UserModel()
+                userModel.setValuesForKeys(snapChild.value as! [String:Any])
+                self.peopleInfoArr.append(userModel)
+            }
+        }
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+}
+ 
