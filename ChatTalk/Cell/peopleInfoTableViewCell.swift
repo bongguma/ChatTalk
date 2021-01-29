4b7e1ed4bc8af38ede0839898652febc69010d39
kimyejin <7602kyj@e-mirim.hs.kr>
2021-01-30 01:04:36 +0900

    tableView 안에 customCell 기능 구현

---
+++ ChatTalk/Cell/peopleInfoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  peopleInfoTableViewCell.swift
+//  ChatTalk
+//
+//  Created by 김예진 on 2021/01/30.
+//
+
+import UIKit
+
+class peopleInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
