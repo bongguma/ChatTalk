4e0ffc112f535f588de2187107a0581c2ba86124
kimyejin <7602kyj@e-mirim.hs.kr>
2021-02-03 22:23:16 +0900

    채팅방 기능 구현을 위한 데이터 생성

---
+++ ChatTalk/Data/ChatModel.swift
@@ -0,0 +1,15 @@
+//
+//  ChatModel.swift
+//  ChatTalk
+//
+//  Created by 김예진 on 2021/02/03.
+//
+
+import UIKit
+
+class ChatModel: NSObject {
+    
+    var uid : String?
+    
+    var destincationUid : String?
+}
