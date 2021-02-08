01801d6347cc3fbeb7903d151a58af838d14a55c
kimyejin <7602kyj@e-mirim.hs.kr>
2021-02-08 23:49:35 +0900

    objectMapper 추가로 맵핑 추가 작업

--- ChatTalk/Data/ChatModel.swift
+++ ChatTalk/Data/ChatModel.swift
@@ -5,17 +5,35 @@
 //  Created by 김예진 on 2021/02/03.
 //
 
-import UIKit
+import ObjectMapper
 
-class ChatModel: NSObject {
+class ChatModel: Mappable {
     
     // 채팅방에 참여한 사람들
     public var users :Dictionary<String, Bool> = [:]
     
+    // 채팅방에 대화내용
     public var comments :Dictionary<String, Comment> = [:]
     
-    public class Comment {
+    
+    required init?(map: Map) {}
+    
+    func mapping(map: Map) {
+        users <- map["users"]
+        comments <- map["comments"]
+    }
+    
+    public class Comment : Mappable {
+        
         public var uid: String?
         public var message: String?
+        
+        
+        public required init?(map: Map) {}
+        
+        public func mapping(map: Map) {
+            uid <- map["uid"]
+            message <- map["message"]
+        }
     }
 }
