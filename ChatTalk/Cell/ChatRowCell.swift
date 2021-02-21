d8d36c9cf5e7cb0df9bd1c6dfff52deec40dcf23
kimyejin <7602kyj@e-mirim.hs.kr>
2021-02-21 23:12:00 +0900

    하단 탭바 채팅방 cell 데이터 삽입 구현

---
+++ ChatTalk/Cell/ChatRowCell.swift
@@ -0,0 +1,52 @@
+//
+//  ChatRowCell.swift
+//  ChatTalk
+//
+//  Created by 김예진 on 2021/02/21.
+//
+
+import UIKit
+import Firebase
+
+class ChatRowCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    @IBOutlet weak var lastMessageLbl: UILabel!
+    
+    @IBOutlet weak var lastMessageTimeLbl: UILabel!
+    
+    var destinationUid : String?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    public func setUiUpdate(_ chatRoom : ChatModel, _ uid : String){
+        for item in chatRoom.users {
+            if (item.key != uid) {
+                destinationUid = item.key
+            }
+        }
+        
+        Database.database().reference().child("users").child(destinationUid!).observeSingleEvent(of: DataEventType.value) { (datasnapshot) in
+            let userModel = UserModel()
+            userModel.setValuesForKeys(datasnapshot.value as! [String:AnyObject])
+            
+            self.titleLbl.text = userModel.name!
+            
+            let lastMesageKey = chatRoom.comments.keys.sorted(){$0>$1}
+            self.lastMessageLbl.text = chatRoom.comments[lastMesageKey[0]]?.message
+            let unixTime = chatRoom.comments[lastMesageKey[0]]?.time
+             
+            self.lastMessageTimeLbl.text = unixTime?.toDayTime
+        }
+    }
+}
--- ChatTalk/TabBar/ChatRoomViewController.swift
+++ ChatTalk/TabBar/ChatRoomViewController.swift
@@ -14,10 +14,13 @@ class ChatRoomViewController: UIViewController, UITableViewDelegate, UITableView
     
     var uid : String?
     var chatRooms : [ChatModel]! = []
+    var destinationUsers : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        self.title = "채팅방"
         self.uid = Auth.auth().currentUser?.uid
         self.getChatRoomList()
     }
@@ -39,9 +42,29 @@ class ChatRoomViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let chatRowCell = tableView.dequeueReusableCell(withIdentifier: "ChatRowCell", for: indexPath)
+        let chatRowCell = tableView.dequeueReusableCell(withIdentifier: "ChatRowCell", for: indexPath) as! ChatRowCell
+        
+        chatRowCell.setUiUpdate(chatRooms[indexPath.row], uid!)
+        
+        var destinationUid : String?
+        
+        for item in chatRooms[indexPath.row].users {
+            if item.key != self.uid {
+                destinationUid = item.key
+                destinationUsers.append(destinationUid!)
+            }
+        }
+        
         return chatRowCell
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let destinationUid = self.destinationUsers[indexPath.row]
+        let chatViewController = self.storyboard?.instantiateViewController(identifier: "ChatViewController") as! ChatViewController
+        chatViewController.destinationUid = destinationUid
+        
+        self.navigationController?.pushViewController(chatViewController, animated: true)
+    }
 }
