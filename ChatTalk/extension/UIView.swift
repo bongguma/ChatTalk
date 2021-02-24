53037b7490f4c1066f038de93dc43832a5ab74a8
kimyejin <7602kyj@e-mirim.hs.kr>
2021-02-24 23:22:18 +0900

    상태메세지 UI 수정

--- ChatTalk/extension/UIView.swift
+++ ChatTalk/extension/UIView.swift
@@ -1,16 +1,16 @@
 //
-//  UIButton.swift
-//  BucketList
+//  UIView.swift
+//  ChatTalk
 //
-//  Created by 김예진 on 2021/01/05.
+//  Created by 김예진 on 2021/02/24.
 //
 
 import UIKit
 
-// UIButton을 확장해 스토리보드 안에서 UI를 추가적으로 꾸밀 수 있다
-@IBDesignable extension UIButton {
+// UIView을 확장해 스토리보드 안에서 UI를 추가적으로 꾸밀 수 있다
+@IBDesignable extension UIView {
 
-    @IBInspectable override var borderWidth: CGFloat {
+    @IBInspectable var borderWidth: CGFloat {
         set {
             layer.borderWidth = newValue
         }
@@ -19,7 +19,7 @@ import UIKit
         }
     }
 
-    @IBInspectable override var cornerRadius: CGFloat {
+    @IBInspectable var cornerRadius: CGFloat {
         set {
             layer.cornerRadius = newValue
         }
@@ -28,7 +28,7 @@ import UIKit
         }
     }
 
-    @IBInspectable override var borderColor: UIColor? {
+    @IBInspectable var borderColor: UIColor? {
         set {
             guard let uiColor = newValue else { return }
             layer.borderColor = uiColor.cgColor
